d61016e97bb47b718f5423ed7229a49d1074c615
Stephen Dowling <x21189994@student.ncirl.ie>
2023-04-05 21:41:14 +0100

    added toggle text feature for plant care page, benefits of plants text

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ S Dowling x21189994@student.ncirl.ie
 04 04 2023
 */
 
+function toggleText(){
+    var dots=document.getElementById("dots");
+    var showMoreText=document.getElementById("moreTextOne");
+    var buttonText=document.getElementById("textButtonOne");
+
+    if (dots.style.display === "none") {
+        showMoreText.style.display = "none";
+        dots.style.display = "inline";
+        buttonText.innerHTML = "Show more";
+    }
+    else {
+        showMoreText.style.display = "inline";
+        dots.style.display = "none";
+        buttonText.innerHTML = "Show less";
+    }
+}
+
+
 function validateName(){
     let fn=document.forms["contactusform"]["fname"].value;
     if(fn==""){
